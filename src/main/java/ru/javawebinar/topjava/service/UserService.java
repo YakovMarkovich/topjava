c173fede1bfbcaef4a7d54ade20d3e61bd3994fc
YakovMarkovich <yakob.markovich@gmail.com>
2020-06-12 17:52:37 +0300

    2 6 dependency injection

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
 
 import java.util.List;
 
