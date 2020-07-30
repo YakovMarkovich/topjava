7063a84ca31dbb8e9e7f47a25c5886092b80dae5
YakovMarkovich <yakob.markovich@gmail.com>
2020-07-30 17:39:24 +0300

    7 16 soapui utf8 converter

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -1,6 +1,6 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
