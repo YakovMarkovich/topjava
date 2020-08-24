0d4e0026b9efce8f922b00a4a57216a295f87d13
YakovMarkovich <yakob.markovich@gmail.com>
2020-08-24 12:29:36 +0300

    11 08 XSS

--- src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -3,12 +3,12 @@ package ru.javawebinar.topjava.web.meal;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.lang.Nullable;
-import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.View;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
 
-import javax.validation.Valid;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
@@ -36,11 +36,8 @@ public class MealUIController extends AbstractMealController {
     }
 
     @PostMapping
-    public ResponseEntity<String> createOrUpdate(@Valid Meal meal, BindingResult result) {
-        if (result.hasErrors()) {
-            // TODO change to exception handler
-            return ValidationUtil.getErrorResponse(result);
-        }
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void createOrUpdate(@Validated(View.Web.class) Meal meal) {
         if (meal.isNew()) {
             super.create(meal);
         } else {
